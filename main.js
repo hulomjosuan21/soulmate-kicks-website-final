ca9b80576c71ef630cd5adc7758ac798b11b7753
Josuan <hulomjosuan@gmail.com>
2024-08-05 20:54:19 +0800

    8/5/24 8pm

--- main.js
+++ main.js
@@ -1,27 +1,21 @@
-import 'animate.css';
+const menuBtn = document.querySelector('.menu-btn');
+const themeBtn = document.querySelector('.theme-btn');
 
-const themeToggle = document.querySelector('.theme-btn');
-const icon = document.querySelector('.bi-brightness-high');
-
-themeToggle?.addEventListener('click', () => {
-  document.body.classList.toggle('theme-dark');
-  icon?.classList.toggle('bi-moon');
-});
+themeBtn.addEventListener('click', (event) => {
+  document.body.classList.toggle('theme-dark')
+})
 
 const overlay = document.querySelector('.overlay');
 
-const closeBtn = document.querySelector('.close-btn');
+menuBtn.addEventListener('click', (event) => {
 
-if(closeBtn){
-  closeBtn.addEventListener('click', () => {
-    if (overlay) {
-      overlay.style.display = 'none';
-    }
-  })
-}
-
-document.querySelector('.list-btn')?.addEventListener('click', () => {
-  if (overlay) {
+  if(overlay) {
     overlay.style.display = 'block';
+  }
+})
+
+document.querySelector('.close-btn').addEventListener('click', (event) => {
+  if(overlay) {
+    overlay.style.display = 'none';
   }
 })
