366fb55ef4585bac8b9031dcd1c4a872cb65794c
Josuan <hulomjosuan@gmail.com>
2024-08-07 10:27:08 +0800

    almost 5.0

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const menuBtn = document.querySelector('.menu-btn');
 const themeBtn = document.querySelector('.theme-btn');
+const themeIcon = document.getElementById('theme-icon');
 
 themeBtn.addEventListener('click', (event) => {
-  document.body.classList.toggle('theme-dark')
-})
+  document.body.classList.toggle('theme-dark');
+
+  if (themeIcon.classList.contains('bi-brightness-high')) {
+    themeIcon.classList.remove('bi-brightness-high');
+    themeIcon.classList.add('bi-moon');
+  } else {
+    localStorage.setItem('theme', 'dark-mode');
+    themeIcon.classList.remove('bi-moon');
+    themeIcon.classList.add('bi-brightness-high');
+  }
+});
 
 const overlay = document.querySelector('.overlay');
 
